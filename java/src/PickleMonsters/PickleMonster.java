52b2d1c02c784a24ccb088b3054263b9c3bca9e1
Zaiqian Ting Chen <ting_chn@yahoo.com>
2016-02-06 00:02:26 -0500

    deez nuts

--- java/src/PickleMonsters/PickleMonster.java
+++ java/src/PickleMonsters/PickleMonster.java
@@ -1,15 +1,24 @@
 package src.PickleMonsters;
+import src.CoordinateSystem;
+
+import java.awt.Image;
+import java.awt.Graphics;
+import java.util.*;
 
 public abstract class PickleMonster {
 
     String name;
     private int health, attackDamage, numPotions;
+    Image pic;
+    CoordinateSystem coordinates;
 
-    public PickleMonster(int h, int ap, int np, String n) {
+    public PickleMonster(int h, int ap, int np, String n, Image p, int x, int y) {
         this.health = h;
         this.attackDamage = ap;
         this.numPotions = np;
         this.name = n;
+        this.pic = p;
+        coordinates = new CoordinateSystem(x, y, p);
     }
 
     public int getHealth(){
@@ -23,6 +32,10 @@ public abstract class PickleMonster {
     }
     public String getName(){
         return name;
+    }
+    public void draw(Graphics g)
+    {
+        coordinates.drawImage(g, pic);
     }
 
     abstract public void kill();
